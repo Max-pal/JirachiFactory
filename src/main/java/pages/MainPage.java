a467131d44a3936a8355f9400c6d402fd2997c66
Kati Eichenbaum <katieichenbaum@gmail.com>
2020-04-28 21:11:44 +0200

    implement logout

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -20,6 +21,12 @@ public class MainPage {
     private WebElement errorMessage;
     @FindBy(xpath = "//a[@id='header-details-user-fullname']")
     private WebElement userProfilePic;
+    @FindBy(id = "log_out")
+    private WebElement logoutButton;
+    @FindBy(xpath = "//strong[contains(text(),'You are now logged out')]")
+    private WebElement logoutMessage;
+    @FindBy(xpath = "//li[@id='user-options']//a[@class='login-link']")
+    private WebElement loginPageLink;
 
     public MainPage(WebDriver driver, WebDriverWait wait) {
         this.wait = wait;
@@ -54,5 +61,22 @@ public class MainPage {
         wait.until(ExpectedConditions.visibilityOf(userProfilePic));
         System.out.println(userProfilePic.getAttribute("data-username"));
         return userProfilePic.getAttribute("data-username");
+    }
+
+    public void logout() {
+        userProfilePic.click();
+        wait.until(ExpectedConditions.visibilityOf(logoutButton));
+        logoutButton.click();
+        wait.until(ExpectedConditions.visibilityOf(logoutMessage));
+    }
+
+    public boolean userIsLoggedOut() {
+        try {
+            loginPageLink.click();
+            wait.until(ExpectedConditions.visibilityOf(userProfilePic));
+            return false;
+        } catch (NoSuchElementException e) {
+            return true;
+        }
     }
 }
