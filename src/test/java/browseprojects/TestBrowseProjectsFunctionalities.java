678f8ed8116616e4a6bf958e76837d56acee015a
Max <max.pal@outlook.com>
2020-04-29 14:44:17 +0200

    Add unfinished test browse project

--- src/test/java/browseprojects/TestBrowseProjectsFunctionalities.java
+++ src/test/java/browseprojects/TestBrowseProjectsFunctionalities.java
@@ -1,4 +1,51 @@
 package browseprojects;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.MainPage;
+import pages.ProjectPage;
+
+import java.util.concurrent.TimeUnit;
+
 public class TestBrowseProjectsFunctionalities {
+
+    private static final String BASEURL = "https://jira.codecool.codecanvas.hu/";
+    private static final String USERNAME = System.getenv("USERNAME");
+    private static final String PASSWORD = System.getenv("PASSWORD");
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+    private static MainPage mainPage;
+    public static ProjectPage projectPage;
+
+    @BeforeAll
+    public static void setup() {
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 10);
+        mainPage = new MainPage(driver, wait);
+        projectPage = new ProjectPage(driver, wait);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(BASEURL);
+        mainPage.login(USERNAME, PASSWORD);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data_project_name.csv")
+    public void BrowseIssueTest(String projectName) {
+        projectPage = new ProjectPage(driver, wait);
+        driver.get(BASEURL + "projects/" + projectName + "/summary");
+        Assertions.assertEquals(projectPage.getProjectNameValue(), projectName);
+    }
+
+    @AfterAll
+    public static void teardown() {
+        driver.close();
+    }
+
 }
