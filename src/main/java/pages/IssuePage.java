5e2f0534d36573f65291279c79d5dfa4a0cd8743
Max <max.pal@outlook.com>
2020-05-25 12:36:42 +0200

    typo fix

--- src/main/java/pages/IssuePage.java
+++ src/main/java/pages/IssuePage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -47,6 +48,7 @@ public class IssuePage {
     public IssuePage(WebDriver driver, WebDriverWait wait) {
         this.wait = wait;
         PageFactory.initElements(driver, this);
+//        AjaxElementLocatorFactory;
     }
     //TODO: Selenium PageFactory AJAX wait library
 
